5f0c89d9a954525c42ebbb65c4832023e52bae99
Kim Tran <trank@cip.ifi.lmu.de>
2018-06-25 14:30:43 +0200

    AmmoText3D

--- Assets/BirdController.cs
+++ Assets/BirdController.cs
@@ -9,7 +9,7 @@ public class BirdController : MonoBehaviour {
 
 	void Start(){
 
-		GameObject d = GameObject.Find ("Canvas/AmmoText3D");
+		GameObject d = GameObject.Find ("Texts/AmmoText3D");
 		ammoText3D = d.GetComponent<TextMesh>();
 	}
 
