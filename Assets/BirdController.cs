b5e72597eeba6b78aab6a0286ee3171b31cc0aea
Fabian Frey <fabian@appdafuer.com>
2018-06-26 13:10:52 +0200

    keine ahnung

--- Assets/BirdController.cs
+++ Assets/BirdController.cs
@@ -7,8 +7,6 @@ public class BirdController : MonoBehaviour {
 	private TextMesh ammoText3D;
 	private ScoreValues ammoV;
 
-	public Leaveblower leaveBlower;
-
 	void Start(){
 
 		GameObject d = GameObject.Find ("Texts/AmmoText3D");
@@ -16,11 +14,12 @@ public class BirdController : MonoBehaviour {
 	}
 
 	public void loadAmmo(){
+		Leaveblower lf = GameObject.Find ("Leaveblower").GetComponent ("Leaveblower") as Leaveblower;
 		Vector3 lbpos = GameObject.Find ("Leaveblower").transform.position;
 		float dis = Vector3.Magnitude (lbpos - transform.position);
 
 		if (dis < 2){
-			leaveBlower.Load ();
+			lf.Load ();
 			Destroy (this.gameObject);
 		}
 	}
